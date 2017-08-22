5d22cb5c1f571cac16ecf2ccadbda88ca3690f12
BiCo26 <bianca.cortes26@gmail.com>
2017-08-22 11:33:50 -0400

    add user-routes

---
+++ routes/user-routes.js
@@ -0,0 +1,10 @@
+
+const express = require('express');
+const userRoutes = express.Router();
+const usersController = require('../controllers/users-controller');
+const authHelpers = require('../services/auth/auth-helpers');
+
+//profile page
+userRoutes.get('/', authHelpers.loginRequired, usersController.index);
+
+module.exports = userRoutes;
