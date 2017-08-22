8b718c10fe718c59d683cc8c48547fb57d376737
Hao Sun <hao.sun@nyu.edu>
2017-08-22 15:50:23 -0400

    add login and register handle in App.js

--- client/src/App.js
+++ client/src/App.js
@@ -5,7 +5,58 @@ import './App.css';
 import GetNews from './components/GetNews';
 import SourcesInput from './components/SourcesInput';
 
+import Login from './components/Login';
+import Register from './components/Register';
+
 class App extends Component {
+   constructor() {
+    super();
+    this.state = {
+      auth: false,
+      user: null,
+    }
+    this.handleLoginSubmit = this.handleLoginSubmit.bind(this);
+    this.handleRegisterSubmit = this.handleRegisterSubmit.bind(this);
+    this.logOut = this.logOut.bind(this);
+   }
+
+  // AUTH
+  handleLoginSubmit(e, username, password) {
+    e.preventDefault();
+    axios.post('/auth/login', {
+      username,
+      password,
+    }).then(res => {
+      this.setState({
+        auth: res.data.auth,
+        user: res.data.user,
+      });
+    }).catch(err => console.log(err));
+  }
+
+  handleRegisterSubmit(e, username, password) {
+    e.preventDefault();
+    axios.post('/auth/register', {
+      username,
+      password,
+    }).then(res => {
+      this.setState({
+        auth: res.data.auth,
+        user: res.data.user,
+      });
+    }).catch(err => console.log(err));
+  }
+
+  logOut() {
+  axios.get('/auth/logout')
+    .then(res => {
+      console.log(res);
+      this.setState({
+        auth: false,
+      });
+    }).catch(err => console.log(err));
+} 
+
   render() {
     return (
       <div className="App">
@@ -16,6 +67,8 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <Login handleLoginSubmit={this.handleLoginSubmit} />
+        <Register handleRegisterSubmit={this.handleRegisterSubmit} />
          <SourcesInput/>
        {/*<GetNews />*/}
       </div>
