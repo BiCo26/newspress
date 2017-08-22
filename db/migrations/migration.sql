105d7dbc2afd42aaa58b84e5caaa68a49ac5982a
BiCo26 <bianca.cortes26@gmail.com>
2017-08-22 10:19:05 -0400

    All

--- db/migrations/migration.sql
+++ db/migrations/migration.sql
@@ -5,3 +5,9 @@ CREATE TABLE IF NOT EXISTS users (
   username VARCHAR(255) UNIQUE NOT NULL,
   password_digest TEXT NOT NULL
 );
+
+bcryptjs
+express-session
+cookie-parser
+passport
+passport-local
