d4b260b0f0148cc9eeefc3a89951d16ba29ae6c1
BiCo26 <bianca.cortes26@gmail.com>
2017-08-23 09:32:12 -0400

    morning all push

--- client/src/components/header.jsx
+++ client/src/components/header.jsx
@@ -1,26 +1,20 @@
+import React from 'react';
 
+const Header = (props) => {
+  return (
+    <header>
+      <div className="logo">NEWSPress</div>
+      <nav>
+        <ul>
+          <li onClick={() => props.setPage('home')}>Home</li>
+          <li onClick={() => props.setPage('login')}>Log In</li>
+          <li onClick={() => props.setPage('register')}>Register</li>
+          <li onClick={() => props.setPage('user')}>User Dashboard</li>
+          <li>Logout</li>
+        </ul>
+      </nav>
+    </header>
+  )
+}
 
-
-// import React from 'react';
-
-// const Header = (props) => {
-//   return (
-//       <header>
-
-//         <div className="logo">NewsPress</div>
-//         <nav>
-//           <ul>
-//             <li className='navLink' onClick={() => props.setPage('home')}>Home</li>
-//             <li className='navLink' onClick={() => props.setPage('#')}>My News</li>
-//             <li className='navLink' onClick={() => props.setPage('login')}>Log In</li>
-//             <li className='navLink' onClick={() => props.setPage('register')}>Register</li>
-//             <li className='navLink' onClick={() => props.setPage('user')}>Dashboard</li>
-//                 <ul className = 'subMenu'>
-//                     <li className="navLink" onClick={props.logOut}>Logout</li>
-//                 </ul>
-//         </nav>
-//       </header>
-//   )
-// }
-
-// export default Header;
+export default Header;
