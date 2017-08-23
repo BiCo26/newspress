79b4cad5829c61fd50002d322547d172b7ed6d09
BiCo26 <bianca.cortes26@gmail.com>
2017-08-23 09:13:42 -0400

    Nav & Logo in Header commented out for merge with Hao

---
+++ client/src/components/header.jsx
@@ -0,0 +1,26 @@
+
+
+
+// import React from 'react';
+
+// const Header = (props) => {
+//   return (
+//       <header>
+
+//         <div className="logo">NewsPress</div>
+//         <nav>
+//           <ul>
+//             <li className='navLink' onClick={() => props.setPage('home')}>Home</li>
+//             <li className='navLink' onClick={() => props.setPage('#')}>My News</li>
+//             <li className='navLink' onClick={() => props.setPage('login')}>Log In</li>
+//             <li className='navLink' onClick={() => props.setPage('register')}>Register</li>
+//             <li className='navLink' onClick={() => props.setPage('user')}>Dashboard</li>
+//                 <ul className = 'subMenu'>
+//                     <li className="navLink" onClick={props.logOut}>Logout</li>
+//                 </ul>
+//         </nav>
+//       </header>
+//   )
+// }
+
+// export default Header;
